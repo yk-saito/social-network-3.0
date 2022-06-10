d76396b338462f6c0f7bb572e7931f9758242e5a
ysaito <shiva.lts239@gmail.com>
2022-06-10 22:03:22 +0900

    refactor(smart-contractor/): Change filename and added comment

--- smart-contractor/contracts/Posting.sol
+++ smart-contractor/contracts/Posting.sol
@@ -5,11 +5,6 @@ pragma solidity ^0.8.4;
 import "hardhat/console.sol";
 
 contract Posting {
-    uint256 totalPosts; // PostPortalコントラクトのストレージに永続的に保存される
-    uint256 private seed; // 乱数生成のシード
-    /**
-     * NewPostイベント
-     */
     event NewPost(address indexed from, uint256 timestamp, string message);
 
     struct Post {
@@ -19,11 +14,12 @@ contract Posting {
     }
 
     Post[] posts;
+    uint256 totalPosts;
+    uint256 private seed; // 乱数生成のシード
 
     // addressと数値を関連づける
     mapping(address => uint256) public lastPostedAt;
 
-    // payableをつけることで、コントラクトに送金機能が実装される
     constructor() payable {
         console.log("PostPortal - Smart Contract!");
 
@@ -39,9 +35,10 @@ contract Posting {
     // スマートコントラクトに含まれる関数を呼び出すには、ユーザーは有効なウォレットを接続する必要がある。
     // -> msg.senderは、誰が関数を呼び出したかを正確に把握し、ユーザー認証を行なっている！
     function post(string memory _message) public {
+        // 現在ユーザーが投稿している時刻と、前回の投稿時刻が15分以上離れていることを確認
         require(
-            lastPostedAt[msg.sender] + 0 seconds < block.timestamp,
-            "Wait 15m"
+            lastPostedAt[msg.sender] + 1 minutes < block.timestamp,
+            "Wait 1m"
         );
         // ユーザーの現在のタイムスタンプを更新
         lastPostedAt[msg.sender] = block.timestamp;
@@ -49,6 +46,7 @@ contract Posting {
         totalPosts += 1;
         console.log("%s postd w/ mwssage %s", msg.sender, _message);
 
+        // 投稿を配列に格納
         posts.push(Post(msg.sender, _message, block.timestamp));
 
         // 乱数生成
@@ -60,18 +58,19 @@ contract Posting {
             // console.log("%s won!", msg.sender);
 
             // 投稿してくれたユーザーに0.0001ETHを送る
-            uint256 prizeAmount = 0.00001 ether;
+            uint256 prizeAmount = 0.0001 ether;
             require(
+                // コントラクトが持つ資金が足りるかチェック
                 prizeAmount <= address(this).balance,
                 "Trying to withdraw more money than the contract has."
             );
             (bool success, ) = (msg.sender).call{value: prizeAmount}("");
             require(success, "Failed to withdraw money from contract.");
         } else {
-            // console.log("%s did not win.", msg.sender);
+            console.log("%s did not win.", msg.sender);
         }
 
-        // コントラクトでemitされたイベントに関する通知をフロントエンド側で取得できるようにする
+        // フロントエンドにイベント発生を通知
         emit NewPost(msg.sender, block.timestamp, _message);
     }
 
@@ -80,7 +79,7 @@ contract Posting {
     }
 
     function getTotalPosts() public view returns (uint256) {
-        // console.log("We have %d total posts!", totalPosts);
+        console.log("total posts: %d", totalPosts);
         return totalPosts;
     }
 }
